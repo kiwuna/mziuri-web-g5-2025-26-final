f69985575ddd33c6dcc33a10118bbe87dd9081bd
nnatroo <nugonatroshvili@gmail.com>
2026-06-09 19:40:59 +0400

    Refactor password toggle button functionality and streamline icon updates

--- public/javascripts/auth.js
+++ public/javascripts/auth.js
@@ -1,15 +1,9 @@
 const togglePasswordBtns = document.querySelectorAll('.toggle-password-btn');
 
-console.log(togglePasswordBtns)
-
 togglePasswordBtns.forEach(btn => {
     btn.addEventListener('click', () => {
         const input = btn.previousElementSibling;
         input.type = input.type === 'password' ? 'text' : 'password';
-        if (input.type === 'password') {
-            btn.src = '/images/show-password-icon.svg';
-        } else {
-            btn.src = '/images/hide-password-icon.svg';
-        }
+        btn.src = input.type === 'password' ? '/images/show-password-icon.svg' : '/images/hide-password-icon.svg';
     })
 })
